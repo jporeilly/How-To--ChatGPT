26286f9424faa3220c051cea295acd827886a321
James O'Reilly <jp.oreilly@hotmail.com>
2023-05-07 15:33:37 +0100

    Update text-vector.py

--- demos/text-vector.py
+++ demos/text-vector.py
@@ -7,7 +7,7 @@ openai.api_key = "OPENAI API KEY"
 
 # define the model and prompt
 model_engine = "text-embedding-ada-002"
-text_prompt = "Hello World!"
+text_prompt = "What is the capital of France"
 
 # encode the text prompt into a vector
 response = openai.Embedding.create(
@@ -20,5 +20,5 @@ response = openai.Embedding.create(
 content = np.array(response.data[0].embedding)
 
 # print the embedding vector
-print("The vector for: Hello World!")
+print("The vector for: What is the capital of France")
 print(content)
