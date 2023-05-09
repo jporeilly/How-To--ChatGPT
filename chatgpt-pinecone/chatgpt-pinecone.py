ea11c97f2f95c3c822d7f04850f4ac3da7e658ed
James O'Reilly <jp.oreilly@hotmail.com>
2023-05-09 14:03:28 +0100

    Update chatgpt-pinecone.py

--- chatgpt-pinecone/chatgpt-pinecone.py
+++ chatgpt-pinecone/chatgpt-pinecone.py
@@ -1,23 +1,24 @@
-# Chatbot for Financial Report
+# Chatbot for Financial Report 2016
 # pip install langchain
-import pinecone
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.vectorstores import Pinecone
 from langchain.embeddings import OpenAIEmbeddings
 import os
+import pinecone
 
 # connect to Pinecone index financial
 pinecone.init(
-    api_key='PINE API KEY',  
+    api_key='PINECONE API KEY',  
     environment='PINECONE REGION'  
 )
 index_name = "financial"
+namespace = "FR_2016"
 
 # create embeddings using OpenAI. Temperature = 0 to stop waffle
-embeddings = OpenAIEmbeddings(openai_api_key='')
+embeddings = OpenAIEmbeddings(openai_api_key='OPENAI API KEY')
 pinecone = Pinecone.from_existing_index(index_name,embeddings)
-openAI = OpenAI(temperature=0, openai_api_key='')
+openAI = OpenAI(temperature=0, openai_api_key='OPENAI API KEY')
 chain = load_qa_chain(openAI, chain_type="stuff")
 
 
